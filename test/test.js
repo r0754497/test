adad71f220eea1f4c32aabaf52aa4133621ee447
Simon <r0754497@student.thomasmore.be>
2022-11-23 14:27:14 +0100

    test update

--- test/test.js
+++ test/test.js
@@ -1,4 +1,5 @@
 const request = require('supertest');
+const fs = require('os');
 
 describe('get campus', () => {
     it('should return first campus', async () => {
@@ -8,7 +9,13 @@ describe('get campus', () => {
         .expect('Content-Type', 'application/json; charset=utf-8')
         .expect((res) => {
             console.log(res.body[0])
-            console.log("test");
+            fs.writeFile("output.json", JSON.stringify(res.body[0]), 'utf8', function (err) {
+                if (err) {
+                    console.log("An error occured while writing JSON Object to file.");
+                    return console.log(err);
+                }
+                console.log("JSON file has been saved.")
+            })
         })
     });
 });
